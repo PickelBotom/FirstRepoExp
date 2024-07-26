30c072cd7562ee0a0bf0b1949d9b29c9ee3717a1
PickelBotom <s220610452@mandela.ac.za>
2024-07-26 17:59:24 +0200

    Added Shebang + entry point ... not sure of entry point but maybe

--- WebScrapping.py
+++ WebScrapping.py
@@ -1,20 +1,20 @@
+#! usr/bin/python3
 from bs4 import BeautifulSoup
 import requests
 
+def scrape():
+    for i in range(2,6):
+        Pg2Scrape = requests.get(f"https://books.toscrape.com/catalogue/category/books_1/page-{i}.html") 
+        soup = BeautifulSoup(Pg2Scrape.text,"html.parser")
+        ol = soup.find("ol")
+        articles= ol.find_all("article",attrs={"class":"product_pod"})
+        for art in articles:
+            img = art.find('img')
+            name= img.attrs['alt']
+            price= art.find("p",attrs={"class":"price_color"}).text
+            rprice = float (price[2:])*19.93
+            output = "{} \tR{:.2f}".format(name,rprice) 
+            print(output)
 
-
-for i in range(2,6):
-    Pg2Scrape = requests.get(f"https://books.toscrape.com/catalogue/category/books_1/page-{i}.html") 
-    soup = BeautifulSoup(Pg2Scrape.text,"html.parser")
-    ol = soup.find("ol")
-    articles= ol.find_all("article",attrs={"class":"product_pod"})
-    for art in articles:
-        img = art.find('img')
-        name= img.attrs['alt']
-        price= art.find("p",attrs={"class":"price_color"}).text
-        rprice = float (price[2:])*19.93
-        output = "{} \tR{:.2f}".format(name,rprice) 
-        print(output)
-
-
-
+if __name__ == '__main__':
+    scrape()
